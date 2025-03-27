a6e29ef8b2581f80037424c34882de0e92c08bea
FrancescoPalazzo97 <francescopalazzo09@gmail.com>
2025-03-27 14:57:54 +0100

    [UPD] Number random generation completed

--- js/script.js
+++ js/script.js
@@ -1,3 +1,6 @@
+// Functions
+const randomNumber = () => Math.floor(Math.random() * 50 + 1) // Genera un numero casuale compreso tra 1 e 50
+
 //Dichiarazione delle varaibili e recupero elementi necessari
 let counter = document.getElementById(`countdown`); // Recupero il countdown e lo assegno alla variabile counter
 let numbersList = document.getElementById(`numbers-list`); // Recupero l'elemento numbers-list e lo assegno a numbersList
@@ -14,3 +17,9 @@ const time = setInterval(function(){ // Creo un intervallo di 1 secondo
 setTimeout (function(){ 
     clearInterval(time);
 }, 30 * 1000)
+
+// Genero i numeri casuali
+for (let i = 0; i < 5; i++){
+    let n = randomNumber();
+    numbersList.innerHTML += `<li>${n}</li>`;
+}
