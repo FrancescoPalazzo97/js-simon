5ec2ae3a5c0ae8000e0d0856f442a3e342eded03
FrancescoPalazzo97 <francescopalazzo09@gmail.com>
2025-03-27 16:17:24 +0100

    [UPD] Improved 'addEventListener' on the button

--- js/script.js
+++ js/script.js
@@ -6,11 +6,15 @@ let counter = document.getElementById(`countdown`); // Recupero il countdown e l
 let numbersList = document.getElementById(`numbers-list`); // Recupero l'elemento numbers-list e lo assegno a numbersList
 let instruction = document.getElementById(`instructions`); // Recupero l'elemento instructions
 let answersForm = document.getElementById(`answers-form`); // Recupero l'elemento answers-form
+let numbers = document.querySelectorAll(`input`); // Recupero gli input del form
+let btn = document.querySelector(`button`); // Reecupero il bottone
+let finalMessage = document.getElementById(`message`); // Recupero l'lemento message
 
 const msPerSecond = 1000;
 
 let count = 30; // Creo una variabile contatore che parte da 30
 counter.innerHTML = count // Inserisco il valore di count all'interno dell'elemento
+let numsGenerated = []; // Creo un array dove metto i numeri generati
 
 const time = setInterval(function(){ // Creo un intervallo di 1 secondo
     count--; // Effettuo il decremento di count
@@ -24,8 +28,9 @@ setTimeout (function(){
 
 // Genero i numeri casuali
 for (let i = 0; i < 5; i++){
-    let n = randomNumber();
-    numbersList.innerHTML += `<li>${n}</li>`;
+    let n = randomNumber(); // Richiamo la funzione e inserisco il valore ritornato nella variabile n
+    numbersList.innerHTML += `<li>${n}</li>`; // Inserisco il numero generato contenuto in un tag <li> nella lista
+    numsGenerated.push(n); // Pusho il numero nell'array
 }
 
 // Creo un'altro timeout per nascondere il contatorre, i numeri e nel mentre modifica il testo dell'elemento instructions e mostra il form dove inserire i numeri
@@ -37,3 +42,26 @@ setTimeout(() => {
     instruction.innerHTML = `<strong>Inserisci i numeri <br> L'ordine non è importante<strong>`;
     answersForm.classList.add(`d-block`);
 }, 31 * msPerSecond);
+
+btn.addEventListener(`click`, (e) => {
+    e.preventDefault();
+    let founded = 0;
+    for (let i = 0; i < numbers.length; i++){
+        let numbersValue = parseInt(numbers[i].value);
+        console.log(`posizione ${i} di numbers: ${numbersValue}`)
+        for (let j = 0; j<numsGenerated.length; j++){
+            console.log(`posizione ${j} di numsGenerated: ${numsGenerated[j]}`)
+            if (numbersValue === numsGenerated[j]){
+                founded++;
+            }
+        }
+    }
+
+    if (founded >= 3){
+        finalMessage.innerHTML = `Hai vinto <br> Hai indovinato ${founded} numeri`
+    } else {
+        finalMessage.innerHTML = `Hai perso <br> Hai indovinato ${founded} numeri`
+    }
+    
+})
+
